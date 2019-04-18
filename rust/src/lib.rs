95c48c989040915eaffeecfc63d0be9e174fb115
Pétur G. Hjartarson <petur.hjartarson@bimobject.com>
2019-04-18 22:51:33 +0200

    slut: sista commiten, lite shady stuff

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -12,8 +12,21 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 extern {
     fn alert(s: &str);
 }
+extern {
+    fn fib(i: &i32);
+}
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, rustlek!");
+}
+
+#[wasm_bindgen]
+#[no_mangle]
+pub fn fib(n: i32) -> i32{
+    match n {
+        0 => 0,
+        1 => 1,
+        _ => fib(n-1) + fib(n-2),
+    }
 }
